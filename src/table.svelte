2c6b8eb06abe303b7b2a34efe8949671cc5348a7
Pepe <173433735+pepe-onche@users.noreply.github.com>
2024-10-10 09:08:14 +0200

    update logo + add share link

--- src/table.svelte
+++ src/table.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { onMount } from 'svelte';
-import { getToastStore } from '@skeletonlabs/skeleton';
+import { getToastStore, clipboard } from '@skeletonlabs/skeleton';
 import { scene } from "./scene/globals";
 import { room, gameState, addDefaultListeners, getNonSpectatorPlayers } from "./stores/colyseus";
 import { isHelpOpen } from './stores/store';
@@ -111,6 +111,12 @@ onMount(async () => {
     await scene.start(getNonSpectatorPlayers(), $gameState.players.get($room.sessionId).hand, $gameState.currentPlayerId, $gameState.turnStartTime, $gameState.deckSize, $gameState.discardPile);
   }
 });
+
+let copyingLink = false;
+function onClipboard() {
+  copyingLink = true;
+  setTimeout(() => copyingLink = false, 3000);
+}
 </script>
 
 <div bind:this={container} class="w-full h-full" style="max-height: 100%">
@@ -122,7 +128,27 @@ onMount(async () => {
       <Win bind:playerName={winnerName}></Win>
     {:else}
       <div class="p-4 flex flex-col items-center">
-        <img src="logo.png" alt="logo" class="w-3/4 py-4 mb-4"/>
+        <img src="logo.png" alt="logo" class="w-3/4 py-4"/>
+        {#if $gameState.players.size < 2}
+          <span class="px-3 py-1 text-red-500 mb-2 font-bold bg-surface-900 rounded-md">Il faut au moins 2 joueurs pour commencer une partie</span>
+        {/if}
+        <div class="w-full md:w-2/4 bg-surface-900 rounded-md text-center p-4 mb-2">
+          <div class="font-bold mb-2">Invitez des joueurs avec ce lien:</div>
+          <div class="flex">
+            <div class="card w-full items-center justify-center text-sm px-4 py-2 mr-2 overflow-hidden" style="text-overflow:ellipsis;white-space:nowrap;">{window.location}</div>
+            <button use:clipboard={window.location} on:click={onClipboard} class="btn-icon btn-icon-sm variant-filled rounded-lg shrink-0">
+              {#if copyingLink}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+                </svg>
+              {:else}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
+                </svg>
+              {/if}
+            </button>
+          </div>
+        </div>
         <button class="btn variant-filled mb-2 w-full md:w-2/4" on:click={() => $isHelpOpen = true}>
           <span>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -132,18 +158,14 @@ onMount(async () => {
           <span>Lire le guide</span>
         </button>
         {#if $room.sessionId === $gameState.kingPlayerId}
-          {#if $gameState.players.size < 2}
-            <span class="px-3 py-1 text-red-500 mb-2 font-bold bg-surface-900 rounded-md">Il faut au moins 2 joueurs pour commencer une partie</span>
-          {:else}
-            <button class="btn variant-filled w-full md:w-2/4" class:opacity-50={$gameState.players.size < 2} class:cursor-not-allowed={$gameState.players.size < 2} on:click={actions.startGame}>
-              <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
-                </svg>
-              </span>
-              <span>Commencer la partie</span>
-            </button>
-          {/if}
+          <button class="btn variant-filled w-full md:w-2/4" class:opacity-50={$gameState.players.size < 2} class:cursor-not-allowed={$gameState.players.size < 2} on:click={actions.startGame}>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+              </svg>
+            </span>
+            <span>Commencer la partie</span>
+          </button>
         {/if}
       </div>
     {/if}
