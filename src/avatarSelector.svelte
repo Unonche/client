c62fd6d6a923990bd3689835fd4a33a02c976f4b
Pepe <173433735+pepe-onche@users.noreply.github.com>
2024-10-19 03:15:42 +0200

    fix avatarSelector padding

--- src/avatarSelector.svelte
+++ src/avatarSelector.svelte
@@ -10,7 +10,7 @@ onMount(() => {
 
 <div class="flex flex-wrap">
 	{#each avatars as a, i}
-	  <div class="w-1/4 p-2 flex items-center justify-center">
+	  <div class="w-1/4 flex items-center justify-center">
 	    <button class="shrink-0 transition-all box-content hover:scale-90 rounded-full overflow-hidden bg-surface-900 border-4 {avatar === a ? 'border-primary-500 scale-90' : 'border-surface-800 scale-75'}" on:click={() => avatar = a}
 	      style="width:100px; height:100px; background-image: url('avatars.png'); background-position: {-i*100}px 0;"
 	    ></button>
